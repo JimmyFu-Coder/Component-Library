05996003497e1b8c97e28176b121acc17c4ad07d
fujinshuai <jinshuai.fu@shanbay.com>
2022-05-29 08:37:17 +0800

    first commit

---
+++ src/components/Button/button.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import Button, {ButtonProps} from '../components/Button/button'
+
+
+export default {
+    title: "Button",
+    component: Button,
+    argTypes: {
+        btnType: {
+            options: ["primary", "danger", 'default', "link"],
+            control: { type: 'radio' },
+        },
+        size:{
+            options: ["lg","sm"],
+            control: { type: 'radio' },
+        }
+    }
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args: ButtonProps) => <Button btnType={"danger"}>Button</Button>
+
+export const Default = Template.bind({})
+Default.args = {
+}
+
+export const DifferentSize = Template.bind({})
+DifferentSize.args = {
+    size:"sm"
+}
+
+export const DifferentType = Template.bind({})
+DifferentType.args = {
+    btnType:"primary"
+}
